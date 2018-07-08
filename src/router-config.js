bd9637a856a79a35a30692a80c27d459125fb2fb
池桂宇 <1092369395@qq.com>
2018-07-08 15:36:11 +0800

    修改个人信息页面

--- src/router-config.js
+++ src/router-config.js
@@ -1,10 +1,11 @@
 import indexPage from './page/newOrExitInfoPage/index.vue'
 import socialPage from './page/socialPage/index.vue'
 import showInfoPage from './page/showInfoPage/yingjie.vue'
+import personalInformation from './page/personal-information.vue'
 
 export default [
   {
-    path: '/1', component: indexPage,
+    path: '/personalInformation', component: personalInformation,
   },
   {
     path:'/2',component: showInfoPage,
